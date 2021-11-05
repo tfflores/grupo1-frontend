a04c79a82fa78d99f08788ce1a92739fdf97c025
tfflores <tfflores@uc.cl>
2021-11-05 13:33:11 -0300

    dummy changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Frontend Grupo 1 TI
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/Components/Ingredient.jsx
+++ src/Components/Ingredient.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+//Commentary 
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
