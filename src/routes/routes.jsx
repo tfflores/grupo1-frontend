c8803eafb0e1b936fd27a5c6c57bca415e98b4df
Tiara Silva <tsilva7@uc.cl>
2021-12-08 17:10:52 -0300

    s

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -9,9 +9,15 @@ export default function RoutesPath() {
   return (
     <Routes>
       <Route exact path="/" element={<Storage />} />
-      <Route path="/grupo1-frontend/" element={<Storage />} />
-      <Route path="/grupo1-frontend/Orders" element={<Orders />} />
-      <Route path="/grupo1-frontend/Storage" element={<Storage />} />
+      <Route path={process.env.PUBLIC_URL} element={<Storage />} />
+      <Route
+        path={process.env.PUBLIC_URL + "/grupo1-frontend/Orders"}
+        element={<Orders />}
+      />
+      <Route
+        path={process.env.PUBLIC_URL + "/grupo1-frontend/Storage"}
+        element={<Storage />}
+      />
     </Routes>
   );
 }
