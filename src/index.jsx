a8a0cb6128e251e515b00d9faeb1f2ace2cc69f6
Tiara Silva <tsilva7@uc.cl>
2021-12-08 17:20:05 -0300

    a

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById("root")
