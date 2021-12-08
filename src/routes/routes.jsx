a27dbc3bf5ed1b25f9c951837a55026717ac6fa7
Tiara Silva <tsilva7@uc.cl>
2021-12-08 18:03:28 -0300

    f

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Routes, Route } from "react-router-dom";
+import Order from "../Components/Order";
 
 import Orders from "../Components/Orders";
 import Storage from "../Components/Storage";
@@ -13,6 +14,9 @@ export default function RoutesPath() {
       <Route path={process.env.PUBLIC_URL + "/Orders"} element={<Orders />} />
       <Route path="/Orders" element={<Orders />} />
       <Route path="/grupo1-frontend/Orders" element={<Orders />} />
+      <Route path="/Orders">
+        <Orders />
+      </Route>
       <Route path={process.env.PUBLIC_URL + "/Storage"} element={<Storage />} />
     </Routes>
   );
